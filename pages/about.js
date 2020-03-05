dd859e8d345612c3ed7f45a5bcdf15e4ffa5e3bf
DNature <divinehycenth@outlook.com>
2020-03-05 12:04:07 +0100

    initialized tailwind and added some styling

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,15 @@
+import Link from 'next/link'
+
 export default function About() {
   return (
-    <div>
-      <p>This is the about page</p>
+    <div className="container mx-auto bg-gray-200 flex flex-col justify-center items-center h-screen">
+      <p className="text-2xl">This is the about page</p>
+
+      <Link href="/">
+        <a className="bg-yellow-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
+          Home
+        </a>
+      </Link>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Link from 'next/link'
+
 export default function Index() {
   return (
-    <div>
-      <p>Hello Next.js</p>
+    <div className="container mx-auto bg-gray-200 flex flex-col justify-center items-center h-screen">
+      <p className="text-4xl">Hello Next.js</p>
+
+      <Link href="/about">
+        <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          About
+        </a>
+      </Link>
     </div>
   )
 }
